a439e65c64c8cfe6370453ff60f439c120b02cfb
said moutaa <smoutaa@gmail.com>
2019-12-10 23:38:42 +0100

    adding uuid  library to generate unique IDs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React ,{useState} from 'react';
+import React ,{useState, useRef} from 'react';
 import TotoList from './TotoList'
+import uuidv4 from 'uuid/v4'
 
 function App() {
  
-  const [todos, setTodos]= useState([{id:1, name: "Crossfit", complete:true}]);
+  const [todos, setTodos]= useState([]);
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e){
+    const name =todoNameRef.current.value
+
+    if(name==='') return
+    setTodos( preTodos => {
+      return [...preTodos, {id:uuidv4(), name: name, complete: false}]
+    })
+    console.log("the name is " + name)
+    todoNameRef.current.value=null
+  }
+
   return (
     <>
        <TotoList todos ={todos}/>
-       <input></input>
-       <button>Add Todo</button>
+       <input ref={todoNameRef} type="text"/> 
+       <button onClick={handleAddTodo}> Add Todo</button>
        <button>Clear Completed</button>
        <div>0 left to do</div>
     </>

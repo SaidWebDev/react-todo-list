1a81aa3c7906ad4f0615e4dbc318a77b9b8048d0
said moutaa <smoutaa@gmail.com>
2019-12-10 23:47:32 +0100

    adding the todos to the localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React ,{useState, useRef} from 'react';
+import React ,{useState, useRef, useEffect} from 'react';
 import TotoList from './TotoList'
 import uuidv4 from 'uuid/v4'
+
+const LOCAL_STORAGE_KEY= 'todoApp.todos'
 
 function App() {
  
   const [todos, setTodos]= useState([]);
   const todoNameRef = useRef()
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]) 
 
   function handleAddTodo(e){
     const name =todoNameRef.current.value
